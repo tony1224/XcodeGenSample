0ebd475586747a580253364ca1ed094033f576b9
kamui_project <starmine2013@gmail.com>
2019-03-16 18:24:41 +0900

    :+1: VCにAlamofireの処理を追加

--- XcodeGenSample/XcodeGenSample/ViewController.swift
+++ XcodeGenSample/XcodeGenSample/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Alamofireをimportしてメソッドが呼べることを確認するための処理です。
+        Alamofire.request("https://connpass.com/api/v1/event/?keyword=ios").responseJSON {
+            res in
+            if res.result.isSuccess {
+                if let returnValue = res.result.value {
+                    print(returnValue)
+                }
+            } else {
+                print("Error!")
+            }
+        }
     }
 
 
